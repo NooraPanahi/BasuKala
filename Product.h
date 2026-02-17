91536277265c8b666a0855dc0f4059708481df35
NooraPanahi <nourap064@gmail.com>
2026-02-17 20:07:34 +0330

    added product class

---
+++ Product.h
@@ -0,0 +1,21 @@
+#ifndef PRODUCT_H
+#define PRODUCT_H
+#include <string>
+
+class Product{
+    private:
+        int categoryId;
+        std::string name;
+        double price;
+        int soldCount;
+    public:
+        Product(int id,const std::string& name , double price);
+        int getId()const;
+        std::string getName() const;
+        double getPrice() const;
+        int getSoldCount() const;
+
+        void increaseSoldCount();
+};
+
+#endif
