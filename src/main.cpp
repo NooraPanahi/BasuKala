66d5d633b6d314fbf71d999a2679d0f2fb6e0785
NooraPanahi <nourap064@gmail.com>
2026-02-18 23:32:00 +0330

    testing basket

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,16 @@
-#include "../include/data_structures/Hash/UserHashTable.h"
+#include "../include/Basket.h"
 #include <iostream>
-int main(){
-    UserHashTable users;
-    users.insert(User(1, "Ali" , 100 , 0));
-    users.insert(User(2 , "ali", 200, 0));
 
-    if( users.exists("Alii"))
-        std::cout << "yes\n";
-    else
-        std::cout << "no\n";
+int main(){
+    Basket basket;
+    basket.addProduct(1 , 56.8);
+    basket.addProduct(2, 32.4);
+    std::cout << "total price: " << basket.getTotalPrice() << std::endl;
+
+   if(basket.removeProductByIndex(1))  //remember: starting from 0
+    std::cout << "total price after removing product 2 : " << basket.getTotalPrice();
+    
+    std::cout << "\nYour products are:\n";
+    for(auto i : basket.getProducts())
+        std::cout << "product id: " << i.productId << " price: " << i.price << '\n'; 
 }
