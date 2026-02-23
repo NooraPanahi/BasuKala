e8cb639eca147945b83d483f4299313b0ab66e18
NooraPanahi <nourap064@gmail.com>
2026-02-23 20:59:21 +0330

    testing trie

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,26 @@
-#include "../include/PurchaseService.h"
+#include <iostream>
 #include <vector>
+#include "../include/data_structures/Trie/Trie.h"
+
 int main(){
-    PurchaseService service;
-   if( service.login("noora"))
-        std::cout <<  "logged in successfully\n"; 
-    else{
-        std::cout <<  " no user found\n";
-        std::cout << "sign in!! \n";
-    }
-    service.registerUser(1,"noora",120);
-    service.login("noora");
-    service.addToBasket(3,6);
-    service.veiwBasket();
-    std::cout << "-------------\n";
-    service.checkout(7,32123);
-    service.showPurchaseHistory();
+
+    Trie trie;
+
+    trie.insert("apple", 1);
+    trie.insert("apricot", 2);
+    trie.insert("application", 3);
+    trie.insert("banana", 4);
+
+    int productId;
+    if (trie.searchExact("apple", productId)){
+        std::cout << "Exact match found. ID = " << productId << "\n";
+    }else
+        std::cout << "Exact match NOT found.\n";
+
+    std::vector<int> results = trie.searchByPrefix("app");
+
+    std::cout << "\nProducts with prefix 'app':\n";
+
+    for (int id : results)
+        std::cout << "ID: " << id << "\n";
 }
