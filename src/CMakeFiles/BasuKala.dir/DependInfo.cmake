17eed03e0d211b633f027fd9c6c42ad99c689699
Mini <mina.zarafshan2013@gmail.com>
2026-02-24 21:18:48 +0330

    debugging

---
+++ src/CMakeFiles/BasuKala.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/mini/Desktop/algo_pro/BasuKala/src/Basket.cpp" "CMakeFiles/BasuKala.dir/Basket.cpp.o" "gcc" "CMakeFiles/BasuKala.dir/Basket.cpp.o.d"
+  "/home/mini/Desktop/algo_pro/BasuKala/src/PurchaseHistory.cpp" "CMakeFiles/BasuKala.dir/PurchaseHistory.cpp.o" "gcc" "CMakeFiles/BasuKala.dir/PurchaseHistory.cpp.o.d"
+  "/home/mini/Desktop/algo_pro/BasuKala/src/PurchaseService.cpp" "CMakeFiles/BasuKala.dir/PurchaseService.cpp.o" "gcc" "CMakeFiles/BasuKala.dir/PurchaseService.cpp.o.d"
+  "/home/mini/Desktop/algo_pro/BasuKala/src/data_structures/Hash/UserHashTable.cpp" "CMakeFiles/BasuKala.dir/data_structures/Hash/UserHashTable.cpp.o" "gcc" "CMakeFiles/BasuKala.dir/data_structures/Hash/UserHashTable.cpp.o.d"
+  "/home/mini/Desktop/algo_pro/BasuKala/src/data_structures/Stack/ItemStack.cpp" "CMakeFiles/BasuKala.dir/data_structures/Stack/ItemStack.cpp.o" "gcc" "CMakeFiles/BasuKala.dir/data_structures/Stack/ItemStack.cpp.o.d"
+  "/home/mini/Desktop/algo_pro/BasuKala/src/data_structures/Trie/Trie.cpp" "CMakeFiles/BasuKala.dir/data_structures/Trie/Trie.cpp.o" "gcc" "CMakeFiles/BasuKala.dir/data_structures/Trie/Trie.cpp.o.d"
+  "/home/mini/Desktop/algo_pro/BasuKala/src/main.cpp" "CMakeFiles/BasuKala.dir/main.cpp.o" "gcc" "CMakeFiles/BasuKala.dir/main.cpp.o.d"
+  "" "BasuKala" "gcc" "CMakeFiles/BasuKala.dir/link.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ src/CMakeFiles/BasuKala.dir/cmake_clean.cmake
@@ -0,0 +1,24 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/BasuKala.dir/link.d"
+  "BasuKala"
+  "BasuKala.pdb"
+  "CMakeFiles/BasuKala.dir/Basket.cpp.o"
+  "CMakeFiles/BasuKala.dir/Basket.cpp.o.d"
+  "CMakeFiles/BasuKala.dir/PurchaseHistory.cpp.o"
+  "CMakeFiles/BasuKala.dir/PurchaseHistory.cpp.o.d"
+  "CMakeFiles/BasuKala.dir/PurchaseService.cpp.o"
+  "CMakeFiles/BasuKala.dir/PurchaseService.cpp.o.d"
+  "CMakeFiles/BasuKala.dir/data_structures/Hash/UserHashTable.cpp.o"
+  "CMakeFiles/BasuKala.dir/data_structures/Hash/UserHashTable.cpp.o.d"
+  "CMakeFiles/BasuKala.dir/data_structures/Stack/ItemStack.cpp.o"
+  "CMakeFiles/BasuKala.dir/data_structures/Stack/ItemStack.cpp.o.d"
+  "CMakeFiles/BasuKala.dir/data_structures/Trie/Trie.cpp.o"
+  "CMakeFiles/BasuKala.dir/data_structures/Trie/Trie.cpp.o.d"
+  "CMakeFiles/BasuKala.dir/main.cpp.o"
+  "CMakeFiles/BasuKala.dir/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BasuKala.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
